6f7e1d0da2848169e50ac3f0cc450409bebbc974
samueltenka <iguavadon@gmail.com>
2015-01-30 22:41:05 -0500

    Update and rename ExprTree to ExprTree.py
    Now able to represent and print parentheses

--- ExprTree.py
+++ ExprTree.py
@@ -8,7 +8,7 @@ class ExprTree:
         else:
             self.op=op; self.pieces=pieces
         self.height = None; self.width = None
-    def __call__(self, atom): pass #use later for parens? ... or should parens be calculated automatically?
+    def __call__(self): return ExprTree('paren', [self]) #use later for parens? ... or should parens be calculated automatically?
     def __add__(self, other): return ExprTree('+', [self,other])
     def __sub__(self, other): return ExprTree('-', [self,other])
     def __mul__(self, other): return ExprTree('*', [self,other])
@@ -19,12 +19,18 @@ class ExprTree:
         if self.op=='atom': self.height = 1
         elif self.op in '+-*': self.height = max(p.get_height() for p in self.pieces)
         elif self.op=='/': self.height = sum(p.get_height() for p in self.pieces) + 1
+        elif self.op=='paren':
+            self.height = self.pieces[0].get_height()
+            if self.height >= 3: self.height += 2
         return self.height
     def get_width(self):
         if self.width: return self.width
         if self.op=='atom': self.width = len(str(self.pieces[0]))
         elif self.op in '+-*': self.width = sum(1+p.get_width() for p in self.pieces) - 1
         elif self.op=='/': self.width = max(p.get_width() for p in self.pieces)
+        elif self.op=='paren':
+            self.width = 2+self.pieces[0].get_width()
+            if self.get_height() >= 3: self.width += 2
         return self.width
 
     def __str__(self):
@@ -45,7 +51,6 @@ class ExprTree:
                     fo, co = floor(offsets[j]), ceil(offsets[j])
                     if fo<=i<h-co: lines[i] += substrs[j][i-fo]
                     else: lines[i] += ' '*wps[j]
-            return '\n'.join(lines)
         elif self.op=='/':
             offsets = [(w-wps[j])/2.0 for j in range(len(self.pieces))]
             i=0
@@ -54,4 +59,11 @@ class ExprTree:
                 fo, co = floor(offsets[j]), ceil(offsets[j])
                 for l in substrs[j]: lines.append(' '*fo + l + ' '*co)
                 i += hps[j]
-            return '\n'.join(lines)
+        elif self.op=='paren':
+            lines = substrs[0]
+            if hps[0] == 1: lines = ['('+lines[0]+')']
+            elif hps[0] == 2: lines = ['/'+lines[0]+'\\', '\\'+lines[1]+'/']
+            elif hps[0] >= 3: lines = [' /'+' '*wps[0]+'\\ '] + \
+                                      ['| '+l+' |' for l in lines] + \
+                                      [' \\'+' '*wps[0]+'/ ']
+        return '\n'.join(lines)
