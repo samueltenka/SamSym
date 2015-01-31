bec5678d286694cee8f7f25679ec6db8ec448474
samueltenka <iguavadon@gmail.com>
2015-01-30 22:27:03 -0500

    Create Main.py
    IT WORKS!

---
+++ Main.py
@@ -0,0 +1,14 @@
+import ExprTree
+
+e8 = ExprTree.ExprTree(8)
+e9 = ExprTree.ExprTree(99)
+ed = (e8+e9)/(e9+e8)
+print(ed+e8)
+#print((ed+ed/ed)/(ed/ed+ed))
+
+'''
+OUTPUT:
+8+99  
+----+8
+99+8 
+'''
