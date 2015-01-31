489a34c776111280191a7bdcdf5e7123050f34d2
samueltenka <iguavadon@gmail.com>
2015-01-30 22:41:24 -0500

    Update Main.py
    Output looks pretty cool.

--- Main.py
+++ Main.py
@@ -3,12 +3,18 @@ import ExprTree
 e8 = ExprTree.ExprTree(8)
 e9 = ExprTree.ExprTree(99)
 ed = (e8+e9)/(e9+e8)
-print(ed+e8)
+print(ed()()()+e8()()())
 #print((ed+ed/ed)/(ed/ed+ed))
 
 '''
 OUTPUT:
-8+99  
-----+8
-99+8 
+ /            \
+|  /        \  |
+| |  /    \  | |
+| | | 8+99 | | |
+| | | ---- | | |+(((8)))
+| | | 99+8 | | |
+| |  \    /  | |
+|  \        /  |
+ \            /
 '''
