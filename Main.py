1169beb57fb965e477a7535d15cfbb58ecaa2960
samueltenka <iguavadon@gmail.com>
2015-01-31 04:51:14 -0500

    Update Main.py
    Quadratic formula can be rendered with a raised exponent!
    
    now for unary minus...

--- Main.py
+++ Main.py
@@ -3,13 +3,17 @@ import ExprTree
 a = ExprTree.ExprTree('a')
 b = ExprTree.ExprTree('b')
 c = ExprTree.ExprTree('c')
-_4 = ExprTree.ExprTree('c')
-_2 = ExprTree.ExprTree('c')
-print(((b*b+_4*a*c).sqrt()-b)/(_2*a))
+_4 = ExprTree.ExprTree(4)
+_2 = ExprTree.ExprTree(2)
+print(((b**_2-_4*a*c).sqrt()()-b)/(_2*a))
+
 '''
 OUTPUT:
-  _________  
-\/b*b+c*a*c-b
--------------
-     c*a     
+ /           \
+|    ________ |
+|   | 2       |-b
+| \/ b -4*a*c |
+ \           /
+-----------------
+       2*a
 '''
