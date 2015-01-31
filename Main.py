371c3965b2e79c24a00d9c192a804b4164b90706
samueltenka <iguavadon@gmail.com>
2015-01-31 05:05:17 -0500

    Update Main.py
    '*' symbol removed from multiplication display. just "ab" instead of "a*b".
    also, unary minus!

--- Main.py
+++ Main.py
@@ -5,15 +5,15 @@ b = ExprTree.ExprTree('b')
 c = ExprTree.ExprTree('c')
 _4 = ExprTree.ExprTree(4)
 _2 = ExprTree.ExprTree(2)
-print(((b**_2-_4*a*c).sqrt()()-b)/(_2*a))
+print((-b+(b**_2+_4*a*c).sqrt())/(_2*a))
 
 '''
 OUTPUT:
- /           \
-|    ________ |
-|   | 2       |-b
-| \/ b -4*a*c |
- \           /
------------------
-       2*a
+ /         \
+|    ______ |
+|   | 2     |-b
+| \/ b -4ac |
+ \         /
+---------------
+      2a
 '''
