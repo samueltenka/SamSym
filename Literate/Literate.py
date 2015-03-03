7a4ca7c516df27c810b7a23b11726f0e36e6f4b0
samueltenka <iguavadon@gmail.com>
2015-03-03 01:05:15 -0500

    Update Literate.py
    Generated by self, given literate source in Literate.lpy

--- Literate/Literate.py
+++ Literate/Literate.py
@@ -1,45 +1,38 @@
 definitions = {}
-
-def learn_from(text):
-    defs = text.split('<<<')
-    for d in defs[1:]:
-        name, rest = d.split('>>>') ## error if multiple ">>>"s
-        name, rest = name.strip(), rest.strip()
-        if name not in definitions:
-            definitions[name] = rest
-        else:
+DEF_START, DEF_END, EXP_START, EXP_END = '<'*3, '>'*3, '<'+'@', '@'+'>'
+START_MACRO = 'START'; MAKE_FILE_NAME = 'make.txt'
+filenames = []
+with open(MAKE_FILE_NAME) as file:
+   filenames = file.read().split('\n')
+out, ins = filenames[0], filenames[1:]
+for filename in ins:
+   with open(filename) as file:
+      text = file.read()
+      sections = text.split(DEF_START)
+      for sect in sections[1:]:
+         name, defn = sect.split(DEF_END) ## error if multiple DEF_END's
+         name, defn = name.strip(), defn.strip()
+         if name in definitions:
             print('may not redefine macro', name)
             exit(-1)
-
-def unweave(start='START'):
-    text = definitions[start]
-    while '<@' in text:
-        new_text = ''
-        lines = text.split('\n')
-        for line in lines:
-            if '<@' in line:
-                spacing = line[:len(line)-len(line.lstrip())]
-                name = line.replace('<@', '').replace('@>', '').strip()
-                if name in definitions:
-                    for defline in definitions[name].split('\n'):
-                        new_text += spacing + defline + '\n'
-                else:
-                    print('macro not found:', name)
-                    exit(-1)
-            else:
-                new_text += line+'\n'
-        text = new_text
-    return text
-
-
-filenames = []
-with open('make.txt') as file:
-    filenames = file.read().split('\n')
-out, ins = filenames[0], filenames[1:]
-
-for filename in ins:
-    with open(filename) as file:
-        learn_from(file.read())
-
+         definitions[name] = defn
 with open(out, 'w') as f:
-    f.write(unweave())
+    text = definitions[START_MACRO]
+    while EXP_START in text:
+       lines = text.split('\n')
+       text = ''
+       for line in lines:
+          if EXP_START in line:
+             spacing = line[:len(line)-len(line.lstrip())]
+             name = line.replace(EXP_START, '').replace(EXP_END, '').strip()
+             if name not in definitions:
+                print('macro not found:', name)
+                exit(-1)
+             for defline in definitions[name].split('\n'):
+                text += spacing + defline + '\n'
+          else:
+              text += line+'\n'
+    f.write(text)
+
+
+
